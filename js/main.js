46d9c2eb3886bd8e540aabfc2152c12fbfbd2890
IvanRizzo28 <22ivanrizzo@gmail.com>
2023-02-22 21:30:27 +0100

    fine milestone2

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 const nome=document.getElementById("nome");
 const km=document.getElementById("km");
 const eta=document.getElementById("eta");
+const div=document.getElementById("biglietto");
 
 const prezzo=0.21;
 let sconto;
-let biglietto="Standard";
+let biglietto;
 
 const genera=document.getElementById("genera");
 const annulla=document.getElementById("annulla");
 
 genera.addEventListener("click", function(){
     sconto=1;
+    biglietto="Standard";
+    div.classList.add("show");
     let prezzoBiglietto=km.value * prezzo;
     if (eta.value==="minorenne") {
         sconto=0.8;
@@ -24,4 +27,14 @@ genera.addEventListener("click", function(){
     prezzoBiglietto=prezzoBiglietto * sconto;
 
     console.log("Il prezzo del biglietto è di: "+prezzoBiglietto.toFixed(2)+"€");
+
+    document.getElementById("nome-passeggero").innerHTML= nome.value;
+    document.getElementById("tipo-biglietto").innerHTML=biglietto;
+    document.getElementById("prezzo").innerHTML=prezzoBiglietto.toFixed(2)+"€";
+});
+
+annulla.addEventListener("click", function(){
+    div.classList.remove("show");
+    nome.value="";
+    km.value="";
 });
